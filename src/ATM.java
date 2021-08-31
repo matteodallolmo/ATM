6c9f97aedc9c6ed4848836cc69ac94951b132aac
Matteo Dall'Olmo <matteo.dallolmo1@gmail.com>
2021-08-31 11:41:43 -0700

    Finished

--- src/ATM.java
+++ src/ATM.java
@@ -1,10 +1,17 @@
+import java.util.*;
 
 public class ATM {
 	
-	[Account] accs = new [Account]();
+	ArrayList<Account> arr = new ArrayList<Account>();
 	
 	public void deposit(String UserID, double amount)
 	{
-		
+		for(int i = 0; i < arr.size(); i++) 
+		{
+			if(arr.get(i).userID.equals(UserID))
+			{
+				arr.get(i).balance += amount;
+			}
+		}
 	}
 }
